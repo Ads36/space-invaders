25423c64cf52a1f4e257143a660f9ecdaae833ae
Ads36 <cervenkaadamek@seznam.cz>
2022-01-31 15:01:31 +0100

    added enemies

--- zapoctak_space_invaders.py
+++ zapoctak_space_invaders.py
@@ -11,4 +11,23 @@ pygame.display.set_caption("Space Invaders by Adam Červenka")
 icon1=pygame.image.load("alien.png")
 pygame.display.set_icon()
 
+enemies_count=10
+enemies=[]
+enemiesX=[]
+enemiesY=[]
+enemiesX_change=[]
+enemiesY_change=[]
+for i in range enemies_count:
+    enemies.append(pygame.image.load("alien.png"))
+    enemiesX.append(random.randint(30,width-30))
+    enemiesY.append(random.randint(20,height//2,5))
+    enemiesX_change.append(2)
+    enemiesY_change.append(20)
+
+
 rocket=pygame.image.load("rocket.png")
+rocketX=width//2
+rocketY=height-120
+rocketX_change=0
+
+
