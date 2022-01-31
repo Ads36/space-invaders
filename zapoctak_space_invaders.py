fdb5303e46a22896a66f2ef1256e1bd154e7dc92
Ads36 <cervenkaadamek@seznam.cz>
2022-01-31 18:12:34 +0100

    added score counter

--- zapoctak_space_invaders.py
+++ zapoctak_space_invaders.py
@@ -7,6 +7,7 @@ width=720
 height=580
 screen=pygame.display.set_mode((width,height))
 black=pygame.Color(0,0,0)
+white=pygame.Color(255,255,255)
 clock=pygame.time.Clock()
 
 pygame.display.set_caption("Space Invaders by Adam Červenka")
@@ -15,19 +16,18 @@ pygame.display.set_icon(icon1)
 
 score=0
 direction=0
-enemies_count=40
+enemies_count=15
 enemies=[]
 enemiesX=[]
 enemiesY=[]
 enemiesX_change=[]
-enemiesY_change=20
+enemiesY_change=50
 for i in range(enemies_count):
     enemies.append(pygame.image.load("alien1.png"))
-    enemiesX.append(random.randint(0,width-128))
-    enemiesY.append(random.randint(20,height//2.5))
+    enemiesX.append(random.randint(0,width-64))
+    enemiesY.append(random.randint(20,height//3))
     enemiesX_change.append(random.choice((-2,2)))
     
-
 rocket=pygame.image.load("rocket1.png")
 rocketX=width//2
 rocketY=height-100
@@ -39,6 +39,10 @@ bulletY=rocketY
 bulletY_change=15
 bullet_fired=False
 
+text=pygame.font.Font("bebas.ttf",30)
+def draw_score(score):
+    score1=text.render("Score: "+str(score),True,white)
+    screen.blit(score1,(15,10))
 def draw_rocket(x,y):
     screen.blit(rocket,(x,y))
 def draw_enemy(which,x,y):
@@ -53,6 +57,10 @@ def enemy_bullet_collision(enemyX,enemyY,bulletX,bulletY):
         return False
 def rocket_bomb_collision(rocketX,rocketY,bombX,bombY):
     distance=math.sqrt(math.pow(rocketX-bombX,2)+math.pow(rocketY-bombY,2))
+    if distance<35:
+        return True
+    else:
+        return False
 
 is_running=True
 while is_running:
@@ -100,7 +108,7 @@ while is_running:
                 bullet_fired=False
                 bulletY=rocketY
                 score+=1
-                enemiesX[which]=random.randint(30,width-30)
+                enemiesX[which]=random.randint(30,width-64)
                 enemiesY[which]=random.randint(20,height//2.5)
 
         draw_enemy(which,enemiesX[which],enemiesY[which])
@@ -112,5 +120,6 @@ while is_running:
         bullet_fired=False
         bulletY=rocketY
     draw_rocket(rocketX,rocketY)
+    draw_score(score)
     clock.tick(60)
     pygame.display.flip()
