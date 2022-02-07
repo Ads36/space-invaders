044d5991a60a75136bab5da580b7c5129e787950
Ads36 <cervenkaadamek@seznam.cz>
2022-02-07 01:02:21 +0100

    english comments

--- zapoctak_space_invaders.py
+++ zapoctak_space_invaders.py
@@ -7,7 +7,7 @@ import math
 import random
 import pygame
 
-#základní nastavování
+#basic settings
 pygame.init()
 width=720
 height=580
@@ -17,38 +17,38 @@ clock=pygame.time.Clock()
 
 pygame.display.set_caption("Space Invaders by Adam Červenka")
 icon1=pygame.image.load("alien1.png")
-pygame.display.set_icon(icon1)     #ikonka programu
-background=pygame.image.load("background1.jfif")    #pozadí
+pygame.display.set_icon(icon1)     #program icon
+background=pygame.image.load("background1.jfif")    #background
 
-#konstanty
-FPS=60              #snímky za sekundu
-enemies_count=9     #počet nepřátel
-enemies_speed=5     #x-ová rychlost nepřátel
-enemiesY_change=50  #y-ový posun nepřátel
-rocketX_change=7    #x-ová rychlost rakety
-bulletY_change=18   #y-ová rychlost střely
-bombsY_change=5     #y-ová rychlost bomby
+#constants
+FPS=55              #frames per second
+enemies_count=9     #number of enemies
+enemies_speed=5     #horizontal velocity of enemies
+enemiesY_change=50  #vertical velocity of enemies
+rocketX_change=7    #horizontal velocity of rocket
+bulletY_change=18   #vertical velocity of rocket
+bombsY_change=5     #vertical veloctiy of bombs
 
 score=0
 best_score=[]
 direction=""
 
-#počáteční nastavení rakety
+#basic settings of rocket
 rocket=pygame.image.load("rocket1.png")
 rocketX=width//2
 rocketY=height-85
 
-#počáteční nastavení střely
+#basic settings of bullet
 bullet=pygame.image.load("bullet2.png")
 bulletX=rocketX
 bulletY=rocketY
 bullet_fired=False
 
-#texty
+#texts
 text1=pygame.font.Font("bebas.ttf",30)
 text2=pygame.font.Font("bebas.ttf",70)
 
-#generování nových nepřátel
+#generating new enemies
 def new_enemies(enemies_count):
     global enemies,enemiesX,enemiesY,enemiesX_change
     enemies=[]
@@ -58,18 +58,20 @@ def new_enemies(enemies_count):
     for _ in range(enemies_count):
         enemies.append(pygame.image.load("alien1.png"))
         enemiesX.append(random.randint(0,width-64))
-        enemiesY.append(random.randint(20,height//3))
+        enemiesY.append(random.randint(20,height//3.5))
         enemiesX_change.append(random.choice((-enemies_speed,enemies_speed)))
-        #získám 4 pole plná údajů o nepřátelích, na jejich základě poté generuji nepřátele
+    #we get 4 arrays full of information of enemies
 
-#funkce na generování bomb, new_bombs vytvoří pouze prádzná pole pro údaje o bombách
+#function for generating bombs
 def new_bombs():
     global bombs,bombs_count,bombsX,bombsY
     bombsX=[]
     bombsY=[]
     bombs=[] 
     bombs_count=0
-#add_bomb se volá po dropnutí bomby, přidá bombu do hry
+
+#add_bomb is called after some enemy drops bomb
+#it adds a bomb to the game
 def add_bomb(x,y):
     global bombs,bombs_count,bombsX,bombsY
     bombs_count+=1
@@ -78,7 +80,7 @@ def add_bomb(x,y):
     bombs.append(pygame.image.load("bomb1.png"))
 
 
-#funkce na vykreslování
+#functions for drawing
 def draw_score(score):
     score1=text1.render("Score: "+str(score),True,white)
     screen.blit(score1,(15,10))
@@ -95,9 +97,8 @@ def draw_bullet(x,y):
 def draw_bomb(which,x,y):
     screen.blit(bombs[which],(x,y))
 
-#funkce na řešení kolizí, vrací True pokud dojde ke kolizi
+#function for collision detection, returns True if a collision happens
 def enemy_bullet_collision(enemyX,enemyY,bulletX,bulletY):
-    #příčítám 3 a 64 kvůli velikosti střely a nepřátel
     if bulletX>=enemyX and bulletX+3<=enemyX+64:
         if abs(enemyY-bulletY)<30:
             return True
@@ -106,7 +107,6 @@ def enemy_bullet_collision(enemyX,enemyY,bulletX,bulletY):
     else:
         return False
 def rocket_bomb_collision(rocketX,rocketY,bombX,bombY):
-    #přičítám 7 a 40 kvůli velikosti bomby a rakety
     if bombX>=rocketX and bombX+7<=rocketX+40:
         if bombY+10>=rocketY:
             return True
@@ -114,19 +114,19 @@ def rocket_bomb_collision(rocketX,rocketY,bombX,bombY):
             return False
     return False
 
-#funkce na konec hry
+#function for game ending
 def game_over():
     global rocketY,is_game_over,bulletY
-    #"zmizení" herních objektů ze screenu
+    #"deletion" of game objects from the screen
     for i in range(enemies_count):
         enemiesY[i]=height+80
     if bullet_fired:
         bulletY=-80
     rocketY=-80
 
-    best_score.append(score)    #přídání score do pole nejlepších skóre
+    best_score.append(score)    #adding score to the best score array
     new_bombs()
-    #vykreslení textu Game over a nejlepšího skóre
+    #drawing text Game over and best score
     end=text2.render("Game Over",True,white)
     restart=text1.render("Press space to restart the game",True,white)
     best_result=text1.render("Your best score is: "+str(max(i for i in best_score)),True,white)
@@ -136,17 +136,17 @@ def game_over():
 
     is_game_over=True
 
-#funkce pro akce nepřátel
+#function for enemies actions
 def enemies_actions():
     global score, bullet_fired, bulletX, bulletY, bombs_count, enemiesX_change 
-    #akce se provede pro všechny nepřátele
+    #for all enemies
     for which in range(enemies_count):
-        #pokud nepřítel dosáhl úrovně rakety - game over
+        #if enemy is lower than rocket - game over
         if enemiesY[which]+40>=rocketY:
             game_over()
             break
 
-        #pohyb nepřátel a odrážení od stěn
+        #bouncing from walls and moving
         enemiesX[which]+=enemiesX_change[which]
         if enemiesX[which]<0:
             enemiesX_change[which]=-enemiesX_change[which]
@@ -155,114 +155,115 @@ def enemies_actions():
             enemiesX_change[which]=-enemiesX_change[which]
             enemiesY[which]+=enemiesY_change
 
-        #dropování bomb, je větší šance že bomba spadne přímo na raketu - aby se hráč musel hýbat
-        bomb_likelyhood=random.randint(0,1000)  #náhodné číslo
-        if enemiesX[which]<=rocketX+10 and enemiesX[which]>=rocketX-10: #pokud je nepřítel nad raketou
-            if bomb_likelyhood>965:     #kontroluji jestli náhodné číslo je větší než určitá mez pro dropnutí bomby
+        #droping bombs, there is a bigger chance to drop a bomb directly onto player
+        bomb_likelyhood=random.randint(0,1000) 
+        if enemiesX[which]<=rocketX+10 and enemiesX[which]>=rocketX-10: #if an enemy is directly above the rocket
+            if bomb_likelyhood>965:     #checking if random number is in an interval
                 add_bomb(enemiesX[which],enemiesY[which])
-        else:       #pokud nepřítel není nad raketou
+        else:                           #if an enemy is not directly above the rocket
             if bomb_likelyhood>997:
                 add_bomb(enemiesX[which],enemiesY[which])
-        #pokud je vystřeleno, tak kontroluji kolize střely a nepřátel
+
+        #if the bullet is fired, we need to check collisions
         if bullet_fired is True:
-            #pokud je nepřítel sestřelen, tak vygeneruji nového a střelu schovám
+            #if an enemy is hit, we generate new one and hide bullet
             if enemy_bullet_collision(enemiesX[which],enemiesY[which],bulletX,bulletY):
                 bullet_fired=False
                 bulletY=rocketY
                 score+=1
                 enemiesX[which]=random.randint(0,width-64)
-                enemiesY[which]=random.randint(20,height//3)
+                enemiesY[which]=random.randint(20,height//3.5)
                 enemiesX_change[which]=random.choice((-enemies_speed,enemies_speed))
 
         draw_enemy(which,enemiesX[which],enemiesY[which])
         
-#funkce pro akce bomb
+#function for bomb actions
 def bombs_actions():
     global bombs_count
-    #akce se provede pro všechny bomby
+    #for all bombs
     for i in range(bombs_count):
-        #None by bylo pokud bomba netrefila raketu a propadla pod okno
+        #None would be, if bomb hadn't hit the rocket
         if bombs[i] is not None:    
-            #vykreslení bomby
+            #drawing of bomb
             draw_bomb(i,bombsX[i],bombsY[i])
             bombsY[i]+=bombsY_change
-            #bomba netrefila raketu
+            #bomb didnť hit rocket
             if bombsY[i]>=rocketY+50:   
                 bombs[i]=None
-            #kontrola kolize, pokud ano, game over    
+            #checking collisions
             if rocket_bomb_collision(rocketX,rocketY,bombsX[i],bombsY[i]):  
                 game_over()
                 break
             
-#funkce pro akce střely
+#function for bullet actions
 def bullet_actions():
     global bullet_fired,bulletX,bulletY
-    #pokud vystřeleno, tak se kreslí střela a letí nahoru
+    #if bullet is fired, bullet is drawn and is flying upwards
     if bullet_fired is True:
         draw_bullet(bulletX,bulletY)
         bulletY-=bulletY_change
-        #kontrola jestli střela nevyletěla mimo
+        #checking if bullet isn't outside the screen
         if bulletY<=0:
             bullet_fired=False
             bulletY=rocketY
 
-#inicializace nepřátel, bomb, běhu hry
+#initialization of enemies, bombs
 new_enemies(enemies_count)
 new_bombs()
 is_running=True
 is_game_over=False
 
-#hlavní chod hry
+#main game cycle
 while is_running:
     screen.blit(background,(0,0))
-    #zjištění stisku kláves
+    #checking keystroke and mouseclick
     for event in pygame.event.get(): 
-        #vypnutí, po stisu křížku   
+        #quiting the game
         if event.type==pygame.QUIT:
             is_running=False            
         if event.type==pygame.KEYDOWN:
-            #ovládání
+            #controlling the rocket
             if event.key==pygame.K_LEFT or event.key==pygame.K_a:   
                 direction="left"
             if event.key==pygame.K_RIGHT or event.key==pygame.K_d:
                 direction="right"
             if event.key==pygame.K_SPACE:
 
-                #nová hra po stisknutí mezerníku
+                #new game after pressing space
                 if is_game_over:        
                     score=0
                     new_enemies(enemies_count)
                     is_game_over=False
                     rocketY=height-90
 
-                #vystřelení    
+                #firing a bullet   
                 if bullet_fired is False and is_game_over is False:     
                     bullet_fired=True
                     bulletX=rocketX+18
 
-        #přerušení pohybu po vymáčknutí klávesy            
+        #stopping the movement after key release
         if event.type==pygame.KEYUP:        
             if event.key==pygame.K_LEFT or event.key==pygame.K_a or event.key==pygame.K_RIGHT or event.key==pygame.K_d:
                 direction=""
 
-    #změna pozice rakety v závislosti na ovládání
+    #change of rocket position
     if direction=="left":
         rocketX-=rocketX_change
     if direction=="right":
         rocketX+=rocketX_change
 
-    #nemožnost rakety dosáhnout pozice mimo okno
+    #rocket can't go beyond screen
     if rocketX>=width-40:
         rocketX=width-40
     if rocketX<=0:
         rocketX=0
 
-    #akce nepřátel, bomb a střely
+    #actions of enemies, bombs and bullet
     enemies_actions()
     bombs_actions()
     bullet_actions()
     
-    #vykreslení a tick
+    #drawing and tick
     draw_rocket(rocketX,rocketY)
     draw_score(score)
     clock.tick(FPS)
